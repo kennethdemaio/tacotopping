be579cdee24a512dc5bca4d7f2a006bc367cf518
Ken <57327247+kennethdemaio@users.noreply.github.com>
2020-05-26 13:03:14 -0400

    Added project
    Added

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.CSharp.RuntimeBinder;
+using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello sir this is taco bell drive through. We currently have a special to make a random taco, would you like this? y/n:");
+            string answer = Console.ReadLine();
+            string[] tacotoppings = { "Meat", "Salsa", "Beans", "Sour cream", "Guacamole", "Tomatoes", "Lettuce" };
+            if (answer == "y")
+            {
+                Random rand = new Random();
+                int index = rand.Next(tacotoppings.Length);
+                Console.WriteLine($"Your taco contains {tacotoppings[index]}");
+            }
+            else _ = (answer == "n");
+                    {
+                Console.WriteLine("Sorry sir, the only taco available is the special taco.");
+
+            }
+            
+
+        }
+    }
+}
